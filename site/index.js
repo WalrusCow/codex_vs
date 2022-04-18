07b0c1525ab55e89ce503beb4efb07725718bc1f
WalrusCow <willgmcd@gmail.com>
2022-04-17 23:50:57 -0700

    Get access token

--- site/index.js
+++ site/index.js
@@ -3,6 +3,7 @@
 const wcl_client_id = '9617066c-1a8b-490a-9442-1a50fc962f97';
 const wcl_auth_uri = 'https://www.warcraftlogs.com/oauth/authorize';
 const wcl_tok_uri = 'https://www.warcraftlogs.com/oauth/token';
+const redirect_uri = 'https://walruscow.github.io/codex_vs/';
 
 async function s256(codeVerifier) {
   const digest = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(codeVerifier));
@@ -10,9 +11,8 @@ async function s256(codeVerifier) {
 }
 
 function generateRandomString(length) {
-  var text = '';
-  // In theory this should also have _-~. characters but I just don't want to deal with it
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_-~.';
+  let text = '';
 
   for (var i = 0; i < length; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
@@ -24,9 +24,43 @@ function generateRandomString(length) {
 class AuthButton extends React.Component {
   constructor(props) {
     super(props);
+    const args = new URLSearchParams(window.location.search);
+    let code = args.get('code');
+
+    if (code !== null) {
+      // we can use code to get a token
+      this.getAccessToken(code);
+    }
+
     this.state = {
       liked: false
     };
+  }
+
+  async getAccessToken(code) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.onload = function () {
+      let response = xhr.response;
+
+      if (xhr.status != 200) {
+        console.log(`Error trying to get access token: ${response.error}. ${response.message}`);
+      }
+
+      window.sessionStorage.setItem('auth_token', response.access_token);
+      console.log('response is ', response);
+    };
+
+    xhr.responseType = 'json';
+    xhr.open('POST', wcl_tok_uri, true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.send(new URLSearchParams({
+      client_id: wcl_client_id,
+      code_verifier: window.sessionStorage.getItem('verifier'),
+      redirect_uri: redirect_uri,
+      grant_type: 'authorization_code',
+      code: code
+    }));
   }
 
   async flub() {
@@ -37,8 +71,7 @@ class AuthButton extends React.Component {
       client_id: wcl_client_id,
       code_challenge: enc_verifier,
       code_challenge_method: 'S256',
-      state: generateRandomString(30),
-      redirect_uri: 'https://walruscow.github.io/codex_vs/',
+      redirect_uri: redirect_uri,
       response_type: 'code'
     });
     console.log('goodbye');
