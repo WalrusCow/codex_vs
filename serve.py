482180a1d386f50613dc32a269556fad424561ac
WalrusCow <willgmcd@gmail.com>
2022-05-06 22:37:56 -0700

    Fancier serve.py

--- serve.py
+++ serve.py
@@ -1,4 +1,6 @@
 import http.server, ssl
+import signal
+from threading import Thread
 
 server_address = ('0.0.0.0', 8000)
 httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
@@ -6,4 +8,9 @@ httpd.socket = ssl.wrap_socket(httpd.socket,
                                server_side=True,
                                certfile='localhost.pem',
                                ssl_version=ssl.PROTOCOL_TLS)
-httpd.serve_forever()
+thread = Thread(target=lambda: httpd.serve_forever())
+thread.start()
+signal.signal(signal.SIGINT, lambda sig, frame: httpd.shutdown())
+print(f'Now listening on :{httpd.server_port}')
+thread.join()
+print(f'Server stopped')
